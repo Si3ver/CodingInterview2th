ec4c8092a38704176ca842c3f1a5c4a24b073318
zhouweilin <zhou_weilin@outlook.com>
2017-06-02 23:58:40 +0800

    Update p26.cpp
    modify and add some description

--- p26.cpp
+++ p26.cpp
@@ -1,13 +1,10 @@
+//经典解法，赋值运算符函数
 CMyString& CMyString::operator = (const CMyString &str)
 {
-	if(this == &str)
-		return *this;
-
+	if(this == &str)	return *this;
 	delete []m_pData;
 	m_pData = nullptr;
-
 	m_pData = new char[strlen(str.m_pData)+1];
 	strcpy(m_pData, str.m_pData);
-
 	return *this;
 }
