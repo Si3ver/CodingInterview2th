cdeebae18151c683306087d1e036b694a8cc1ce2
zhouweilin <zhou_weilin@outlook.com>
2017-06-03 23:36:28 +0800

    Add files via upload
    面试题1

---
+++ t3_1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+bool duplicate(int numbers[], int length, int* duplication)
+{
+	if (numbers == nullptr || length <= 0){
+		return false;
+	}
+
+	for (int i = 0; i < length; i++){
+		if (numbers[i] < 0 || numbers[i] > length - 1){
+			return false;
+		}
+	}
+
+	for (int i = 0; i < length; i++){
+		while(numbers[i] != i){
+			if (numbers[i] == numbers[numbers[i]]){
+				*duplication = numbers[i];
+				return true;
+			}
+
+			//swap numbers[i] & numbers[numbers[i]]
+			// int tmp = numbers[numbers[i]];
+			// numbers[numbers[i]] = numbers[i];
+			// numbers[i] = tmp;
+			int tmp = numbers[i];
+			numbers[i] = numbers[tmp];
+			numbers[tmp] = tmp;
+		}
+	}
+}
+
+
+int main()
+{
+	int data[] = {2, 3, 1, 0, 2, 5, 3};
+	int dumplication = -1;
+	if (duplicate(data, 7, &dumplication)){
+		cout<<dumplication<<endl;
+	}else{
+		cout<<"no duplication"<<endl;
+	}
+	return 0;
+}
