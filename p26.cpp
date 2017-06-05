739667f68840f27a8bfd68579e16b086902253f7
zhouweilin <zhou_weilin@outlook.com>
2017-06-05 23:49:03 +0800

    Update p26.cpp

--- p26.cpp
+++ p26.cpp
@@ -1,7 +1,9 @@
 //经典解法，赋值运算符函数
 CMyString& CMyString::operator = (const CMyString &str)
 {
-	if(this == &str)	return *this;
+	if(this == &str){
+		return *this;
+	}
 	delete []m_pData;
 	m_pData = nullptr;
 	m_pData = new char[strlen(str.m_pData)+1];
