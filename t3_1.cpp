7cc9efb28befb556bd8e4106875bd83074f749e0
zhouweilin <zhou_weilin@outlook.com>
2017-06-13 22:16:05 +0800

    Create t3_1.cpp

--- t3_1.cpp
+++ t3_1.cpp
@@ -1,4 +1,7 @@
-/*寻找0~n-1数组中重复的数
+/*题目一：找出数组中重复的数字。
+数组长度为n，所有数字在0~n-1范围内。可能存在某些数字重复，若有重复的数字，返回true，并找出任意一个重复的数字，否则，返回false。
+思路：扫描一遍，把每个数字放入该放的位置（交换）~
+测试用例：numbers[7] = {3, 1, 2, 0, 2, 5, 3}, length = 7, 函数应返回true, 重复的数字为2or3;
 */
 
 #include <iostream>
@@ -6,16 +9,19 @@ using namespace std;
 
 bool duplicate(int numbers[], int length, int* duplication)
 {
+	//检查数组长度
 	if (numbers == nullptr || length <= 0){
 		return false;
 	}
 
+	//检查数组合法性
 	for (int i = 0; i < length; i++){
 		if (numbers[i] < 0 || numbers[i] > length - 1){
 			return false;
 		}
 	}
 
+	//遍历一遍数组，把元素放入“合理位置”
 	for (int i = 0; i < length; i++){
 		while(numbers[i] != i){
 			if (numbers[i] == numbers[numbers[i]]){
